f20e89ebd1e301280a3b4936ac9b44653be4e3bc
umesh-creates <thefreetouseemail@gmail.com>
2026-08-08 22:27:39 -0400

    order_items entity - order_item_id, product_id, quantity generation code written

--- generator/order_items.py
+++ generator/order_items.py
@@ -1,6 +1,23 @@
+print('wait...')
+import pandas as pd
+import random
 '''
 order_item_id (PK) 1 to 1000 series
-order_id (FK) random between (1 to 1000)
-product_id (FK) random betwenn (1 to 50)
+order_id (FK) random between (1 to 1000) {orders}
+product_id (FK) random betwenn (1 to 50) {products}
 quantity random between (1 to 10)
 '''
+order_items = {
+    'order_item_id' : [f"ORD_ITEM{i:02d}" for i in range(1, 1001)],
+    'order_id' : [],
+    'product_id' : [],
+    'quantity' : []
+}
+
+for _ in range(1000):
+    rand_product_id = random.randrange(1,51)
+    order_items['product_id'].append(f"P_ID{rand_product_id:02d}")
+
+for _ in range(1000):
+    rand_quantity = random.randrange(1,11)
+    order_items['quantity'].append(rand_quantity)
